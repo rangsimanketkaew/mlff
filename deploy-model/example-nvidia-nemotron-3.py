3a3beb9e50af0b48ae20526a5b095cf8bfa27088
Rangsiman Ketkaew <rangsiman1993@gmail.com>
2026-07-14 10:17:43 +0200

    Update query

--- deploy-model/example-nvidia-nemotron-3.py
+++ deploy-model/example-nvidia-nemotron-3.py
@@ -8,14 +8,20 @@ client = OpenAI(
     base_url="https://integrate.api.nvidia.com/v1",
     api_key=os.getenv(
         "NVIDIA_API_KEY",
-        # "YOUR_API_KEY_HERE"
+        # "YOUR_NVIDIA_API_KEY"
     ),
 )
 
 
 completion = client.chat.completions.create(
     model="nvidia/nemotron-3-nano-omni-30b-a3b-reasoning",
-    messages=[{"role": "user", "content": ""}],
+    messages=[
+        {
+            "role": "user",
+            "content": "What's the most accurate MLIP model for computing \
+                the energy of a strongly-corrected system?",
+        }
+    ],
     temperature=0.6,
     top_p=0.95,
     max_tokens=65536,
